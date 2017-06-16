55dbd29bdd002d76f69b083a9f8f1a757f58620d
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-06-15 23:49:55 -0400

    Make GetComments only get comments with a specific parent comment or post

--- server/api.go
+++ server/api.go
@@ -2,6 +2,7 @@ package server
 
 import (
     "fmt"
+    "errors"
     "net/http"
     "database/sql"
     "encoding/json"
@@ -16,7 +17,7 @@ func SetupAPI(r web.Router, db *sql.DB) {
     r.HandleRoute([]string{web.GET}, "/comments", GetComments, db)
 }
 
-func GetPosts(w http.ResponseWriter, queryParams map[string][]string, body string, db *sql.DB) {
+func GetPosts(w http.ResponseWriter, q map[string][]string, b string, db *sql.DB) {
     var err error
     defer func() {
         if err != nil {
@@ -31,22 +32,36 @@ func GetPosts(w http.ResponseWriter, queryParams map[string][]string, body strin
     if err != nil {
         return
     }
-    b, err := json.Marshal(posts)
+    res, err := json.Marshal(posts)
     if err != nil {
         return
     }
     w.Header().Set("Content-Type", "application/json")
-    w.Write(b)
+    w.Write(res)
 }
 
-func GetComments(w http.ResponseWriter, queryParams map[string][]string, body string, db *sql.DB) {
+func GetComments(w http.ResponseWriter, q map[string][]string, b string, db *sql.DB) {
     var err error
     defer func() {
         if err != nil {
             http.Error(w, err.Error(), http.StatusInternalServerError)
         }
     }()
-    rows, err := db.Query(fmt.Sprintf("SELECT %s FROM Comments", models.CommentSQLColumns))
+    pPosts, hasPPost := q["parentPost"]
+    pComments, hasPComment := q["parentComment"]
+    var query string
+    var parent string
+    if (!hasPPost || len(pPosts) == 0) && (!hasPComment || len(pComments) == 0) {
+        err = errors.New("Require parentPost or parentComment")
+        return;
+    } else if hasPComment {
+        query = "SELECT %s FROM Comments WHERE ParentComment=%s"
+        parent = pComments[0]
+    } else {
+        query = "SELECT %s FROM Comments WHERE ParentPost=%s"
+        parent = pPosts[0]
+    }
+    rows, err := db.Query(fmt.Sprintf(query, models.CommentSQLColumns, parent))
     if err != nil {
         return
     }
@@ -54,10 +69,10 @@ func GetComments(w http.ResponseWriter, queryParams map[string][]string, body st
     if err != nil {
         return
     }
-    b, err := json.Marshal(posts)
+    res, err := json.Marshal(posts)
     if err != nil {
         return
     }
     w.Header().Set("Content-Type", "application/json")
-    w.Write(b)
+    w.Write(res)
 }
