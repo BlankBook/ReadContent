f931124cf20bb6b2aa26f9f3ffc71dc98b6dea68
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-06-16 23:20:38 -0400

    Add endpoint to get ID for making contributions to posts

--- server/api.go
+++ server/api.go
@@ -2,7 +2,6 @@ package server
 
 import (
     "fmt"
-    "errors"
     "net/http"
     "database/sql"
     "encoding/json"
@@ -15,6 +14,9 @@ import (
 func SetupAPI(r web.Router, db *sql.DB) {
     r.HandleRoute([]string{web.GET}, "/posts", GetPosts, db)
     r.HandleRoute([]string{web.GET}, "/comments", GetComments, db)
+    // Get /contributorid gets a unique ID that must be used for all 
+    // contributions the user makes to this post
+    r.HandleRoute([]string{web.GET}, "/contributorid", GetContributorId, db)
 }
 
 func GetPosts(w http.ResponseWriter, q map[string][]string, b string, db *sql.DB) {
@@ -51,8 +53,8 @@ func GetComments(w http.ResponseWriter, q map[string][]string, b string, db *sql
     pComments, hasPComment := q["parentComment"]
     var query string
     var parent string
-    if (!hasPPost || len(pPosts) == 0) && (!hasPComment || len(pComments) == 0) {
-        err = errors.New("Require parentPost or parentComment")
+    if !hasPPost || len(pPosts) == 0 {
+        http.Error(w, "Query parameter parentPost required", http.StatusBadRequest)
         return;
     } else if hasPComment {
         query = "SELECT %s FROM Comments WHERE ParentComment=%s"
@@ -75,4 +77,34 @@ func GetComments(w http.ResponseWriter, q map[string][]string, b string, db *sql
     }
     w.Header().Set("Content-Type", "application/json")
     w.Write(res)
+}
+
+func GetContributorId(w http.ResponseWriter, q map[string][]string, b string, db *sql.DB) {
+    postIds, hasPostId := q["postId"]
+    if !hasPostId || len(postIds) == 0 {
+        http.Error(w, "Query parameter postId required", http.StatusBadRequest)
+        return
+    }
+    query := `
+        UPDATE TOP (1) Posts
+        SET NextUserID = NextUserID + 1
+        OUTPUT INSERTED.NextUserID
+        WHERE ID = %s
+       `
+    idrow, err := db.Query(fmt.Sprintf(query, postIds[0]))
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    var id int
+    idrow.Next()
+    err = idrow.Scan(&id)
+    idrow.Close()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    res := fmt.Sprintf(`{"id":%d}`, id)
+    w.Header().Set("Content-Type", "application/json")
+    w.Write([]byte(res))
 }
